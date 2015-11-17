54b83f210d34316dbc6190b34efec35ef421f12c
haidj9901 <haidj9901@HRH-IC0027335.wrdsb.ca>
2015-11-17 11:19:53 -0500

    i

---
+++ src/hhssadventure/Scene.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hhssadventure;
+
+/**
+ *
+ * @author haidj9901
+ */
+public class Scene {
+    
+    //private BufferedImage;
+    //private Direction;
+    //private Location;
+    private boolean frontBlocked;
+    public Scene()
+    {
+        frontBlocked = false;
+    }
+          
+    public boolean isFrontBlocked()
+    {
+        return (frontBlocked == true);
+    }
+    
+    public Location
+}
