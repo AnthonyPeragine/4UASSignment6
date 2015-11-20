f6a2f11cc1fa73e3e387aea34a127a18d09fab9f
haidj9901 <haidj9901@HRH-IC0027335.wrdsb.ca>
2015-11-20 10:52:25 -0500

    i

--- src/hhssadventure/HHSSAdventure.java
+++ src/hhssadventure/HHSSAdventure.java
@@ -5,11 +5,32 @@
  */
 package hhssadventure;
 
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
- * @author lamon
+ * @author jaf
  */
 public class HHSSAdventure {
+
+    ArrayList<Scene> scenes = new ArrayList<Scene>();
+
+    public HHSSAdventure() {
+        FileReader file = null;
+        try {
+            file = new FileReader("images/pics.txt");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        Scanner input = new Scanner(file);
+        while (input.hasNext()) {
+            Scene s = new Scene(input);
+            //pass the next line into the location class
+            scenes.add(s);
+        }
+    }
 
     /**
      * @param args the command line arguments
@@ -17,5 +38,4 @@ public class HHSSAdventure {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
 }
---
+++ src/hhssadventure/Location.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hhssadventure;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author haidj9901
+ */
+public class Location {
+    
+    
+    
+    public Location(Scanner s)
+    {
+    }
+}
