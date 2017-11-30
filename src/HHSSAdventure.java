d2cd90027dfe714ba35af9ca91d227755cc309c4
peraa0837 <peraa0837@HRH-IC0027324.wrdsb.ca>
2017-11-30 09:16:16 -0500

    Initial Commit for Weekend work

--- src/HHSSAdventure.java
+++ src/HHSSAdventure.java
@@ -1,3 +1,7 @@
+
+import java.io.FileReader;
+import java.net.URL;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -14,5 +18,11 @@ public class HHSSAdventure {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        TextFileReader file = new TextFileReader();
+        
+        System.out.println(file.getSpawn() + " " + file.getSpawnDir());
+        for(int i = 0; i < file.locations.size(); i++)
+            System.out.println(file.locations.get(i).getDir());
     }
 }
---
+++ src/Location.java
@@ -0,0 +1,75 @@
+
+import java.awt.image.BufferedImage;
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+*
+* @author peraa0837
+*/
+public class Location {
+    private String name;
+    private String direction;
+    private String img;
+    private boolean wall;
+    private String nextLoc;
+    private String nextDir;
+    
+
+    //construct using the name
+    public Location(String name){
+        this.name = name;
+    }
+    
+    //SETTERS
+    public void setDir(String dir){
+        this.direction = dir;
+    }
+    
+    public void setImage(String img){
+        this.img = img;
+    }
+    
+    public void setWall(boolean wall){
+        this.wall = wall;
+    }
+    
+    public void setNextLoc(String nextLoc){
+        if(wall){
+            this.nextLoc = nextLoc;
+        }
+    }
+    
+    public void setNextDir(String nextDir){
+        if(wall){
+            this.nextDir = nextDir;
+        }
+    }
+    
+    //GETTERS
+    public String getDir(){
+        return this.direction;
+    }
+    
+    public String getImage(){
+        return this.img;
+    }
+    
+    public boolean getWall(){
+        return this.wall;
+    }
+    
+    public String getNextLoc(){
+        return this.nextLoc;
+    }
+    
+    public String getNextDir(){
+        return this.nextDir;
+    }
+}
+
+
